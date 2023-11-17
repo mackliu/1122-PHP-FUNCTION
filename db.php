f954209d76d1ca3e926e1fdc17b00268cefb5efa
kai <cgk0910@gmail.com>
2023-11-17 14:19:31 +0800

    在函式中來增加參數,提升泛用性

--- db.php
+++ db.php
@@ -1,17 +1,21 @@
 <?php
 
-$rows=all();
+$rows=all('dept');
 
 dd($rows);
 
 
-function all(){
+function all($table=null){
     $dsn="mysql:host=localhost;charset=utf8;dbname=school";
     $pdo=new PDO($dsn,'root','');
 
-    $sql="select * from `students` ";
-    $rows=$pdo->query($sql)->fetchAll();
-    return $rows;
+    if(isset($table) && !empty($table)){
+        $sql="select * from `$table` ";
+        $rows=$pdo->query($sql)->fetchAll();
+        return $rows;
+    }else{
+        echo "錯誤:沒有指定的資料表名稱";
+    }
 }
 
 
