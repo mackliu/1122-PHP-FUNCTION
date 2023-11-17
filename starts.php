9dc75dfd1e4a08b4356619df21b36e315d50f44d
kai <cgk0910@gmail.com>
2023-11-17 13:37:33 +0800

    利用自訂函式來簡化畫星星的程式及用法

--- starts.php
+++ starts.php
@@ -7,10 +7,14 @@
 <?php
 
 equilateral_triangle(5);
-equilateral_triangle(9);
-equilateral_triangle(13);
-
-
+//equilateral_triangle(9);
+//equilateral_triangle(13);
+diamond(5);
+//diamond(7);
+//diamond(9);
+retangle(5);
+retangle(7);
+retangle(9);
 function equilateral_triangle($size){
     for($i=0;$i<$size;$i++){
         for($j=0;$j<($size-1-$i);$j++){
@@ -23,3 +27,41 @@ function equilateral_triangle($size){
     }
 }
 
+function diamond($size){
+$mid=floor(($size*2 -1)/2);
+$tmp=0;
+    for($i=0;$i<($size * 2 -1);$i++){
+
+        if($i<=$mid){
+            $tmp=$i;
+        }else{
+            $tmp=$tmp-1;
+        }
+
+        for($j=0;$j<($mid-$tmp);$j++){
+            echo "&nbsp;";
+        }
+        for($k=0;$k<($tmp*2+1);$k++){
+            echo "*";
+        }
+        echo "<br>";
+    }
+}
+
+
+function retangle($size){
+    for($i=0;$i<$size;$i++){
+
+        for($j=0;$j<$size;$j++){
+            if($i==0 || $i==($size-1)){
+                echo "*";
+            }else if($j==0 || $j==($size-1)){
+                echo "*";
+            }else{
+                echo "&nbsp;";
+            }
+    
+        }
+        echo "<br>";
+    }
+}
