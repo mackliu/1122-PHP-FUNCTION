ae7c557d75e002c3a93a75812e5fa22d43108c75
kai <cgk0910@gmail.com>
2023-11-17 11:24:40 +0800

    不定參數的自訂函式範例

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<h1>自訂函式</h1>
 <?php
 //自訂函式
 $c=20;
@@ -17,6 +18,36 @@ echo "總和是：".$sum;
 echo "<hr>";
 
 echo "總和是:".sum(56,77);
+?>
 
+<h2>不定參數的用法</h2>
+<?php
+function sum2(...$arg){
+    $sum=0;
+    foreach($arg as $num){
+        if(is_numeric($num)){
+            $sum +=$num;
+        }
+
+    }
+
+    return $sum;
+}
+
+
+echo sum2(1,2);
+echo "<hr>";
+echo sum2(23,45,89);
+echo "<hr>";
+echo sum2(23,98,34,89,22,3,4);
+echo "<hr>";
 
 ?>
+
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
