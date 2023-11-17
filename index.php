4a36018bbb6e6abb51ae4d4e1b1da9757d0b5624
kai <cgk0910@gmail.com>
2023-11-17 10:54:43 +0800

    自訂函式基礎

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//自訂函式
+$c=20;
+function sum($a,$b){
+    global $c;
+    $sum=$a+$b+$c;
+    echo "輸入:".$a."、".$b;
+    echo "<br>";
+    return $sum;
+}
+
+$sum=sum(10,20);
+echo "總和是：".$sum;
+echo "<hr>";
+$sum=sum(17,22);
+echo "總和是：".$sum;
+echo "<hr>";
+
+echo "總和是:".sum(56,77);
+
+
+?>
